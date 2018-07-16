c0610e3d29e060abeaabc431487b05ed74216910
Zach Lysobey <zach.lysobey@alphapoint.com>
2018-07-15 23:40:01 -0400

    initial commit

---
+++ src/pages/music.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => (
+    <div>
+        <h2>Music</h2>
+        <Link to="/">back home</Link>
+    </div>
+)
---
+++ src/pages/programming.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => (
+    <div>
+        <h2>Programming</h2>
+        <Link to="/">back home</Link>
+    </div>
+)

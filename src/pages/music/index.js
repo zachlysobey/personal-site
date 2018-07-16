9eb713d951013f2094df8bf076a6d1e513dfc3c1
Zach Lysobey <zach.lysobey@alphapoint.com>
2018-07-16 00:22:03 -0400

    add some content

--- src/pages/music/index.js
+++ src/pages/music/index.js
@@ -1,9 +1,17 @@
 import React from "react"
 import Link from "gatsby-link"
 
+import { YoutubePosts } from './youtube-posts'
+import { Bands } from './bands'
+
 export default () => (
     <div>
         <h2>Music</h2>
+
+        <YoutubePosts></YoutubePosts>
+
+        <Bands></Bands>
+
         <Link to="/">back home</Link>
     </div>
 )
--- src/pages/programming/index.js
+++ src/pages/programming/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import Link from "gatsby-link"
 
+import { ExternalArticles } from './external-articles'
+
 export default () => (
     <div>
         <h2>Programming</h2>
+
+        <ExternalArticles></ExternalArticles>
+
         <Link to="/">back home</Link>
     </div>
 )

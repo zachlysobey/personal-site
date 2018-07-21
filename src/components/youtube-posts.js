976caed61437ff848e08391971a20531f2e92922
Zach Lysobey <zach.lysobey@alphapoint.com>
2018-07-21 17:34:51 -0400

    fix: add extra space for weird text display bug

--- src/components/youtube-posts.js
+++ src/components/youtube-posts.js
@@ -10,7 +10,7 @@ export const YoutubePosts = () => (
       I'm gonna try to post a new YouTube video every day. It'll be mostly
       original stuff but mostly just ideas and song fragments. The goal is to
       start producing
-      <em>something</em> everyday at least.
+      <em> something</em> everyday at least.
     </p>
 
     <ul>

159e2b4d9b92b25943282213542d232938f103f6
Zach Lysobey <zach.lysobey@alphapoint.com>
2018-07-22 19:04:06 -0400

    display: move bands above youtube vids in music/

--- src/pages/music.js
+++ src/pages/music.js
@@ -8,9 +8,9 @@ export default () => (
     <div>
         <h2>Music</h2>
 
-        <YoutubePosts />
-
         <Bands />
+
+        <YoutubePosts />
 
         <Link to="/">back home</Link>
     </div>

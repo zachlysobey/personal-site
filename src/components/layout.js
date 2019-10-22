139b21f85cba45b52c1832f7547c7378183a24b7
Zach Lysobey <zlysobey@gmail.com>
2019-10-21 23:50:23 -0400

    (display) auto-hide header with react-headroom

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
+import Headroom from 'react-headroom'
 
 import Header from './header'
 
@@ -36,10 +37,12 @@ const Layout = ({ children, data }) => (
                         { name: 'keywords', content: 'sample, something' },
                     ]}
                 />
-                <Header siteTitle={data.site.siteMetadata.title} />
+                <Headroom>
+                    <Header siteTitle={data.site.siteMetadata.title} />
+                </Headroom>
                 <div
                     style={{
-                        margin: '0 auto',
+                        margin: '1.45rem auto 0',
                         maxWidth: 960,
                         padding: '0px 1.0875rem 1.45rem',
                         paddingTop: 0,

63c10b9d79712242d8e5aa8a50a07cb749f6935b
Zach Lysobey <zlysobey@gmail.com>
2019-11-15 00:44:39 -0500

    📈 add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,23 @@ module.exports = {
     plugins: [
         'gatsby-plugin-react-helmet',
         {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: 'UA-29116435-1',
+                head: false,
+                anonymize: true,
+                respectDNT: true,
+                exclude: [],
+                pageTransitionDelay: 0,
+                // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+                // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+                // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: 'auto',
+            },
+        },
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 path: `${__dirname}/src/pages`,
